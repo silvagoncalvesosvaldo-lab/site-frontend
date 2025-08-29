0fa38b64af4e5dc41bd5242ff1671ab16208352e
unknown <silvagoncalvesosvaldo@gmail.com>
2025-08-29 12:51:58 -0300

    build atualizado para produção

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  },
+  preview: {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  },
 })
