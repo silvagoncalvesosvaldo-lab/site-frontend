ef7b43a65611f5a186eb76ebbcd4ec06378ebccd
unknown <silvagoncalvesosvaldo@gmail.com>
2025-09-22 19:52:36 -0300

    chore(vite): simplificar config (remover plugins que externalizam deps)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração padrão do Vite para projetos React
 export default defineConfig({
   plugins: [react()],
 })
